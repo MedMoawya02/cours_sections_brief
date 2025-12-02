ab116640b73a2a74127a7c3aa8e57bccdbfd649a
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2025-12-02 09:44:12 +0100

    feat:l'ajout de fichier config.php pour la connection entre le db

---
+++ infrastructure/config.php
@@ -0,0 +1,12 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="courses";
+$conn=new mysqli($servername,$username,$password,$dbname);
+if($conn->connect_error){
+    echo"Connection failed".$conn->connect_error;
+}else{
+    echo"Connection with success";
+}
+?>
