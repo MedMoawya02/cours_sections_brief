4d05836db2a7374ca1a329492d2ae705f15ffab9
MedMoawya02 <mohammed.mouawya2002@gmail.com>
2025-12-02 11:23:58 +0100

    feat:l'ajout du page courses_list.php ,pour lister tous les données depuis la db

---
+++ cours/courses_list.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootsrtap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- icons link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Courses</title>
+
+    <style>
+        table{
+            text-align: center;
+        }
+        td a{
+            text-decoration: none;
+            color: #fff;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include '../infrastructure/config.php'; ?>
+    <div class="container my-5">
+        <button class="btn btn-primary"><i class="fa-solid fa-plus"></i>New course</button>
+        <table class="table table-hover mt-3">
+            <tr>
+                <th>id</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Level</th>
+                <th>Actions</th>
+            </tr>
+            <?php
+            if ($conn->connect_error) {
+                echo "connection failed" . $conn->connect_error;
+            }
+            $sql = "SELECT * FROM course";
+            $result = $conn->query($sql);
+            if ($result && $result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                     ?>
+                   <tr>
+                        <td><?php echo $row['course_id']?></td>
+                        <td><?php echo $row['title']?></td>
+                        <td><?php echo $row['description']?></td>
+                        <td><?php echo $row['niveu']?></td>
+                        <td><button class="btn btn-success"><a href="">Edit</a></button>
+                        <button class="btn btn-danger"><a href="">Delete</a></button>
+                    </td> 
+                   </tr>
+                   <?php
+                }
+            }else{
+                ?>
+                <tr>
+                    <td colspan="4" style="text-align: center;">No records found.</td>
+                </tr>
+                <?php
+            }
+           ?>
+
+        </table>
+    </div>
+</body>
+
+</html>
--- infrastructure/config.php
+++ infrastructure/config.php
@@ -6,7 +6,7 @@ $dbname="courses";
 $conn=new mysqli($servername,$username,$password,$dbname);
 if($conn->connect_error){
     echo"Connection failed".$conn->connect_error;
-}else{
+}/* else{
     echo"Connection with success";
-}
+} */
 ?>
